40e9e16ee95bef135bfac7601ae641bfcfa69cee
Ian Hammond <hammy4815@gmail.com>
2022-02-08 13:42:07 -0700

    still testing

--- emepy/__init__.py
+++ emepy/__init__.py
@@ -7,3 +7,7 @@ Eigenmode Expansion Python
 __version__ = "0.2.4"
 __author__ = "Ian Hammond"
 __credits__ = "BYU CamachoLab"
+
+from emepy.eme import *
+from emepy.tools import *
+from emepy.mode import *
