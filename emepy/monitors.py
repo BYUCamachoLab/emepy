8563754c84a3ef834e4b284a581c7f44e7ec96fc
Ian Hammond <hammy4815@gmail.com>
2022-02-22 20:35:57 -0700

    docs for monitors and source

--- emepy/monitors.py
+++ emepy/monitors.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib
 from copy import deepcopy
 from scipy.interpolate import griddata
 
@@ -9,38 +10,41 @@ class Monitor(object):
 
     def __init__(
         self,
-        axes="xz",
-        dimensions=(1, 1),
-        lengths=[],
-        components=["E"],
-        z_range=None,
-        grid_x=None,
-        grid_y=None,
-        grid_z=None,
-        location=None,
-        remaining_lengths=[],
-        sources=[],
-    ):
+        axes:str="xz",
+        dimensions:tuple=(1, 1),
+        lengths:list=[],
+        components:list=["E"],
+        z_range:tuple=None,
+        grid_x:np.array=None,
+        grid_y:np.array=None,
+        grid_z:np.array=None,
+        location:float=None,
+        sources:list=[],
+    ) -> None:
         """Monitor class constructor0
 
         Parameters
         ----------
         axes : string
-            the spacial axes to capture fields in. Options : 'xz' (default), 'xy', 'yz', 'xyz', 'x', 'y', 'z'. Note, propagation is always in z.
+            the spacial axes to capture fields in. Options : 'xz' (default), 'xy', 'yz', 'xyz', 'x', 'y', 'z'. Note, propagation is always in z. (default: "xy")
         dimensions : tuple
-            the spacial dimensions of the resulting field
+            the spacial dimensions of the resulting field (default: (1,1))
         lengths : list
-            list of the remaining points in z at which to calculate the fields
+            list of the remaining points in z at which to calculate the fields (default: [])
         components : list
-            list of the field components to store from ('E','H','Ex','Ey','Ez','Hx','Hy','Hz)
+            list of the field components to store from ('E','H','Ex','Ey','Ez','Hx','Hy','Hz) (default: ["E"])
         z_range : tuple
-            tuple or list of the form (start, end) representing the range of the z values to extract
-        grid_x : numpy array
+            tuple or list of the form (start, end) representing the range of the z values to extract (default: None) 
+        grid_x : numpy array (default: None)
             1d x grid
-        grid_y : numpy array
+        grid_y : numpy array (default: None)
             1d y grid
-        grid_z : numpy array
+        grid_z : numpy array (default: None)
             1d z grid
+        location : float
+            the location in z if the monitor represents "xy" axes (default: None)
+        sources : list[Source]
+            sources to use for the monitor (default:[])
         """
 
         # Ensure z range is in proper format
@@ -57,6 +61,7 @@ class Monitor(object):
                     "and end are floats such as (0, 1e-6) for a 1 µm range"
                 ) from e
 
+        # Check axes
         if axes == "xz" or axes == "zx":
             self.axes = "xz"
         elif axes == "yz" or axes == "zy":
@@ -71,51 +76,54 @@ class Monitor(object):
                 " the following implemented monitor types: ['xy','yz','xz','xyz']"
             )
             
+        # Set parameters globally
         self.dimensions = dimensions
         self.field = np.zeros(dimensions).astype(complex)
         self.lengths = deepcopy(lengths)
         self.sources = sources
         self.left_source = False
         self.right_source = False
-
         self.components = components
         self.layers = {}
         self.grid_x = grid_x
         self.grid_y = grid_y
         self.grid_z = grid_z
+        self.location = location
 
-    def reset_monitor(self):
+    def reset_monitor(self) -> None:
+        """Resets the fields in the monitor"""
         self.field *= 0
 
-    def soft_reset(self):
-        self.remaining_lengths = deepcopy(self.remaining_lengths_reset)
+    def get_z_list(self, start:float, end:float) -> list:
+        """Finds all the points in z between start and end
+        
+        Parameters
+        ----------
+        start : float
+            starting point in z
+        end : float
+            ending point in z
 
-    def __getitem__(self, subscript):
-        return self.field[subscript]
-
-    def __setitem__(self, subscript, item):
-
-        # Only set item if it's in the valid z_range
-        self.field[subscript] = item
-
-    def __delitem__(self, subscript):
-        del self.field[subscript]
-
-    def get_z_list(self, start, end):
+        Returns
+        -------
+        list[tuples]
+            A list of tuples that take the format (i, l) where i is the index of the z point and l is the z point for all z points in the range
+        """
         return [(i, l) for i, l in enumerate(self.lengths[0]) if start <= l <= end]
 
-    def normalize(self):
+    def normalize(self) -> None:
+        """Normalizes the entire field to 1"""
         self.field[:-1] /= 1  # np.max(np.abs(self.field[:-1, :, 0]))
 
-    def get_array(self, component="Hy", axes=None, location=None, z_range=None, grid_x=None, grid_y=None):
+    def get_array(self, component:str="Hy", axes:str=None, location:float=None, z_range:tuple=None, grid_x:np.array=None, grid_y:np.array=None) -> np.array:
         """Creates a matplotlib axis displaying the provides field component
 
         Parameters
         ----------
-        axes : string
-            the spacial axes to capture fields in. Options : 'xz' (default), 'xy', 'yz', 'xyz', 'x', 'y', 'z'. Note, propagation is always in z.
-        component : string
+        component : str
             field component from "['Ex','Ey','Ez','Hx','Hy','Hz','E','H']"
+        axes : str
+            the spacial axes to capture fields in. Options : 'xz' (default), 'xy', 'yz', 'xyz', 'x', 'y', 'z'. Note, propagation is always in z.
         location : float
             if taken from 3D fields, users can specify where to take their 2D slice. If axes is 'xz', location refers to the location in y and 'yz' refers to a location in x and 'xy' refers to a location in z
         z_range : tuple
@@ -129,7 +137,6 @@ class Monitor(object):
         -------
         numpy array
             the requested field
-
         """
 
         # Default axes is created upon class creation
@@ -308,7 +315,8 @@ class Monitor(object):
 
         return grid_field
 
-    def get_source_visual(self):
+    def get_source_visual(self) -> np.array:
+        """Returns a mask with lines indicating where a source is"""
         z = self.lengths[:]
         array = np.zeros(self.dimensions[1:])
         l = self.dimensions[-1] // 200
@@ -321,25 +329,32 @@ class Monitor(object):
             i = self.lengths[0].index(min(self.lengths[0], key=difference_start))
             array[:,i-l//2:i+l//2] = 1
 
-            pass
         return array
 
-    def visualize(self, ax=None, component="Hy", axes=None, location=0, z_range=None, show_geometry=True, show_sources=True):
+    def visualize(self, ax:matplotlib.image.AxesImage=None, component:str="Hy", axes:str=None, location:float=0, z_range:tuple=None, show_geometry:bool=True, show_sources:bool=True) -> matplotlib.image.AxesImage:
         """Creates a matplotlib axis displaying the provides field component
 
         Parameters
         ----------
-        axes : string
-            the spacial axes to capture fields in. Options : 'xz' (default), 'xy', 'yz', 'xyz', 'x', 'y', 'z'. Note, propagation is always in z.
         ax : matplotlib axis
             the axis object created when calling plt.figure() or plt.subplots(), if None (default) then the plt interface will be used
         component : string
             field component from "['Ex','Ey','Ez','Hx','Hy','Hz','E','H']"
+        axes : string
+            the spacial axes to capture fields in. Options : 'xz' (default), 'xy', 'yz', 'xyz', 'x', 'y', 'z'. Note, propagation is always in z.
         location : float
             if taken from 3D fields, users can specify where to take their 2D slice. If axes is 'xz', location refers to the location in y and 'yz' refers to a location in x and 'xy' refers to a location in z.
         z_range : tuple
             tuple or list of the form (start, end) representing the range of the z values to extract
+        show_geometry : bool
+            if true, will display the geometry faintly under the field profiles (default: True)
+        show_sources : bool
+            if true, will display a red line indicating where a source is (default: True)
 
+        Returns
+        -------
+        matplotlib.image.AxesImage
+            the image used to plot the index profile
         """
 
         # Only 2D fields can be generated
@@ -437,3 +452,12 @@ class Monitor(object):
                 plt.title(component)
 
         return im
+
+    def __getitem__(self, subscript):
+        return self.field[subscript]
+
+    def __setitem__(self, subscript, item):
+        self.field[subscript] = item
+
+    def __delitem__(self, subscript):
+        del self.field[subscript]
--- emepy/source.py
+++ emepy/source.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 class Source(object):
     """This class defines mode sources that can be created for monitors"""
 
-    def __init__(self, z:int=None, mode_coeffs:list=[], k=1):
+    def __init__(self, z:float=None, mode_coeffs:list=[], k=1) -> None:
         """Constructor for Source. Note: if the modes corresponding to the coefficients defined are not found in the system, the coefficients will be reduced to 0. For now, the wavelength will always be the same as defined in EME.
 
         Parameters
@@ -17,24 +17,35 @@ class Source(object):
             if defined, the z location to define each mode source (default: None)
         mode_coeffs : list[int]
             if z is non-empty, this list represents the modes used for each source. (default: [])
-        k : boolean
-            if positve, will propagate in the positive direction
+        k : int
+            if positive (>0), will propagate in the positive direction
         """
 
         self.z = z
         self.mode_coeffs = mode_coeffs
         self.k = k > 0
 
-    def get_label(self):
+    def get_label(self) -> str:
+        """Returns a string that represents the source"""
         k = "+" if self.k else "-"
         return "{}{}".format(k,self.z)
 
-    def match_label(self, label):
+    def match_label(self, label:str) -> bool:
+        """Sees if the provided label matches the current Source object's and returns True if so
+        
+        Parameters
+        ----------
+        label : str
+            the label to compare
+
+        Returns
+        -------
+        bool
+            True if label matches self
+
+        """
         return label == self.get_label()
 
-
-    def verify(self):
-        """Parses self to ensure proper format
-        """
-        return True
+    def __str__(self):
+        return self.get_label()
 
